72498d40e1a2e1ff53270acb484bd0f0767ad2f3
Mads Stengaard Jørgensen <au592631@uni.au.dk>
2019-11-21 22:07:38 +0100

    Light test

--- Microwave.IntegrationTest/T2Light.cs
+++ Microwave.IntegrationTest/T2Light.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,46 @@ namespace IntegrationTest
             _utt = new Light(_output);
         }
 
+        [Test]
+        public void Output_LightIsOn()
+        {
+            //Act
+            string Output;
+            _output.OutputLine("Light is turned on");
 
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                _utt.TurnOn();
+
+                Output = sw.ToString();
+            }
+
+            //Assert
+            Assert.That(Output, Is.EqualTo("Light is turned on\r\n"));
+        }
+
+        [Test]
+        public void Output_LightIsOff()
+        {
+            //Act
+            _utt.TurnOn();
+            string Output;
+            _output.OutputLine("Light is turned off");
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                _utt.TurnOff();
+
+                Output = sw.ToString();
+            }
+
+            //Assert
+            Assert.That(Output, Is.EqualTo("Light is turned off\r\n"));
+
+        }
     }
 }
