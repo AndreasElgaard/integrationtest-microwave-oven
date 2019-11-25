0eb9e23e42e814565ed5c98b3745efcd33efdd3e
Elgaard1 <andreas.e.sorensen@hotmail.com>
2019-11-25 09:58:20 +0100

    powertube og display

--- Microwave.IntegrationTest/T1Display.cs
+++ Microwave.IntegrationTest/T1Display.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NSubstitute;
@@ -13,25 +15,65 @@ namespace IntegrationTest
     public class T1Display
     {
         private IOutput _output;
-        private Display _utt;
+        private Display _uut;
 
         [SetUp]
         public void Setup()
         {
             _output = new Output();
-            _utt = new Display(_output);
+            _uut = new Display(_output);
         }
 
-        [TestCase(1, 20)]
-        public void ShowTime(int min, int sec)
+        [TestCase(59, 59)]
+        //[Test]
+        public void Test_ShowTimeIsWhatExpected(int min, int sec)
         {
+            string output;
+            //int min = 59;
+            //int sec = 59; 
             _output.OutputLine($"Display shows: {min:D2}:{sec:D2}");
 
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                _uut.ShowTime(min, sec);
+                output = stringWriter.ToString();
+            }
 
-            Assert.AreEqual(_utt.);
+            Assert.That(output, Is.EqualTo($"Display shows: {min}:{sec}\r\n"));
         }
 
+        [TestCase(2)]
+        //[Test]
+        public void Test_ShowPower_Is_what_expected(int power)
+        {
+            string output;
+            //int power = 2; 
+            _output.OutputLine($"Display shows: {power} W");
 
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                _uut.ShowPower(power);
+                output = stringWriter.ToString();
+            }
 
+            Assert.That(output, Is.EqualTo($"Display shows: {power} W\r\n"));
+        }
+        [Test]
+        public void Test_Clear_Is_what_expected()
+        {
+            string output;
+            _output.OutputLine($"Display cleared");
+
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                _uut.Clear();
+                output = stringWriter.ToString();
+            }
+
+            Assert.That(output, Is.EqualTo($"Display cleared\r\n"));
+        }
     }
 }
---
+++ Microwave.IntegrationTest/T3PowerTube.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+
+namespace Microwave.IntegrationTest
+{
+    class T3PowerTube
+    {
+        private IOutput _output;
+        private PowerTube _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new Output();
+            _uut = new PowerTube(_output);
+        }
+
+        [TestCase(5)]
+        //[Test]
+        public void Test_Test_turnOn(int power)
+        {
+            string output;
+            _uut.TurnOn(power);
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                _uut.TurnOn(power);
+                output = stringWriter.ToString();
+            }
+
+            Assert.That(output, Is.EqualTo($"PowerTube.TurnOn: is already on\r\n"));
+
+        }
+    }
+}
